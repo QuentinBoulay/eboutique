4321b6795db40d824c21159cd224d600fb6f0f3d
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-02-21 19:21:18 +0100

    Affichage des produits disponibles sur la Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupérer les produits depuis la base de données
+        $products = $entityManager->getRepository(Product::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $products,
         ]);
     }
 }
