53dac0247fa5b50fc3860aba72f39fc8c83384a7
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-03-04 15:33:06 +0100

    Ajout catégories page Boutique

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Product;
+use App\Entity\Category;
 
 
 class ShopController extends AbstractController
@@ -14,12 +15,14 @@ class ShopController extends AbstractController
     #[Route('/shop', name: 'app_shop')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        // Récupérer les produits depuis la base de données
+        // Récupérer les produits et catégories depuis la base de données
         $products = $entityManager->getRepository(Product::class)->findAll();
+        $categories = $entityManager->getRepository(Category::class)->findAll();
 
         return $this->render('shop/index.html.twig', [
             'controller_name' => 'ShopController',
             'products' => $products,
+            'categories' => $categories,
         ]);
     }
 }
