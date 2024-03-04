7fbd0a4e2b805dc7c08375565cfdc882ea732954
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-03-04 14:43:35 +0100

    Ajout catégories Home + Affichage erreur register

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,17 +7,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Product;
+use App\Entity\Category;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        // Récupérer les produits depuis la base de données
+        // Récupérer les produits et catégories depuis la base de données
         $products = $entityManager->getRepository(Product::class)->findAll();
+        $categories = $entityManager->getRepository(Category::class)->findAll();
 
         return $this->render('home/index.html.twig', [
             'products' => $products,
+            'categories' => $categories,
         ]);
     }
 }
